df9f6d982376532d08253f8de3e7ec2d46f4fa2e
yinyong <yinyong@kuaishou.com>
2018-11-15 21:55:53 +0800

    1.0.0

--- README.md
+++ README.md
@@ -1,31 +1,30 @@
-#insight
-[![Bower version][bower-image]][bower-url]
+# insight
 
 Help you create web pages as the design sketch.It draws a half-opacity design sketch on the page,then you could see where is wrong.
 
 It supports all pc browsers including IE6/IE7,but we have a problem on IE6:it does not support position:fixed,so the ctrl-panel won't be fixed on NE/NW/SW/SE position.
 
-##Usage
-#####HTML
-	<script src="./js/insight.js"></script>
-	<script >insight(imageURL,options);</script>
+## Usage
 
-#####options:
+##### HTML
 
-1. **css**: css attributes
-2.**bigStep**: step when you enter shift key and 'AWSD'
-6. **ctrlPanelPosition**: 'NE', //Or NW or SW or SE
-7. **hideonload**: true,
-8. **hotkeysEnabled**: true,
-9. **navButtonsEnabled**: true
+    <script src="./js/insight.js"></script>
+    <script >insight(imageURL,options);</script>
 
-##Example
+##### options:
 
-	insight('/static/img/index.test.png',{bigStep:10,css:{width:"100%",height:"1800px",zIndex:10000}});
+1. **css**: css attributes 2.**bigStep**: step when you enter shift key and 'AWSD'
+2. **ctrlPanelPosition**: 'NE', //Or NW or SW or SE
+3. **hideonload**: true,
+4. **hotkeysEnabled**: true,
+5. **navButtonsEnabled**: true
 
-##Changelog
- - 2014-11-15[18:25:57]:register to bower
- - 2014-01-03[23:40:27]:support big step
+## Example
 
-[bower-url]:http://badge.fury.io/bo/insight
-[bower-image]: https://badge.fury.io/bo/insight.svg
+    insight('/static/img/index.test.png',{bigStep:10,css:{width:"100%",height:"1800px",zIndex:10000}});
+
+## Changelog
+
+- 2018-11-15[21:54:39]:umd;remove bower
+- 2014-11-15[18:25:57]:register to bower
+- 2014-01-03[23:40:27]:support big step
--- insight.js
+++ insight.js
@@ -1,193 +1,210 @@
 /**
-  * yy.insight-0.1.0.js
-  *
-  * changelog
-  * 2013-12-31[16:11:51]:copyright
-  *
-  * @info yinyong,osx-x64,UTF-8,10.129.172.183,js,/Volumes/yinyong/daohangqq2/resource/views/scripts/insight
-  * @author yanni4night#gmail.com
-  * @version 0.1.1
-  * @since 0.1.0
-  */
-var insight = function(imgSrc, options) {
-    options = options || {};
-    var defaultOptions = {
-        zIndex: 10000,
-        ctrlPanelPosition: 'NE', //Or NW or SW or SE
-        hideonload: true,
-        hotkeysEnabled: true,
-        navButtonsEnabled: true,
-        bigStep:5,
-        css:{}
-    };
+ * yy.insight-1.0.0.js
+ *
+ * changelog
+ * 2013-12-31[16:11:51]:copyright
+ * 2018-11-15[21:53:47]:umd
+ *
+ * @author yanni4night#gmail.com
+ * @version 1.0.0
+ * @since 0.1.0
+ */
+export default function insight(imgSrc, options) {
+  options = options || {};
+  var defaultOptions = {
+    zIndex: 10000,
+    ctrlPanelPosition: "NE", //Or NW or SW or SE
+    hideonload: true,
+    hotkeysEnabled: true,
+    navButtonsEnabled: true,
+    bigStep: 5,
+    css: {}
+  };
 
-    function getStyle(ele,name){
-        return window.getComputedStyle?window.getComputedStyle(ele,null)[name]:ele.currentStyle[name];
+  function getStyle(ele, name) {
+    return window.getComputedStyle
+      ? window.getComputedStyle(ele, null)[name]
+      : ele.currentStyle[name];
+  }
+
+  function bindEvent(ele, evt, func) {
+    if (document.addEventListener) {
+      ele.addEventListener(evt, func, false);
+    } else {
+      ele.attachEvent("on" + evt, func);
+    }
+  }
+
+  //Load options
+  for (var i in defaultOptions) {
+    options[i] = undefined === options[i] ? defaultOptions[i] : options[i];
+  }
+  //Check options.ctrlPanelPosition
+  if (!/(NE|NW|SW|SE)/.test(options.ctrlPanelPosition)) {
+    options.ctrlPanelPosition = options.ctrlPanelPosition.ctrlPanelPosition;
+  }
+
+  //Half-opacity div
+  var div = document.createElement("div");
+
+  div.style.display = options.hideonload ? "none" : "block";
+  div.style.position = "absolute";
+
+  div.style.zIndex = options.css.zIndex || 10000;
+  div.style.width = options.css.width || "100%";
+  div.style.height = options.css.height || "100%";
+  div.style.left = 0;
+  div.style.top = 0;
+  div.style.background = "url(" + imgSrc + ") left top no-repeat";
+  for (var e in options.css) {
+    if (options.css.hasOwnProperty(e)) {
+      div.style[e] = options.css[e];
+    }
+  }
+
+  //Show or hide
+  var toggle = function() {
+    if ("none" == div.style.display) {
+      div.style.display = "block";
+      for (var i in navBtns) {
+        navBtns[i].disabled = "";
+      }
+    } else {
+      div.style.display = "none";
+      for (var i in navBtns) {
+        navBtns[i].disabled = "disabled";
+      }
+    }
+  };
+  //With buttons
+  var ctrldiv = document.createElement("div");
+  var ctrldivPosCssText;
+  switch (options.ctrlPanelPosition) {
+    case "NE":
+      ctrldivPosCssText = "right:0;top:0;";
+      break;
+    case "NW":
+      ctrldivPosCssText = "left:0;top:0;";
+      break;
+    case "SE":
+      ctrldivPosCssText = "right:0;bottom:0;";
+      break;
+    case "SW":
+      ctrldivPosCssText = "left:0;bottom:0;";
+      break;
+  }
+  ctrldiv.style.cssText =
+    "width:100px;height:100px;position:fixed;_position:absolute;" +
+    ctrldivPosCssText +
+    "z-index:" +
+    ((1 + options.zIndex) | 0);
+
+  var navBtnConfig = [
+    {
+      left: 0,
+      top: 35,
+      text: "A",
+      cmd: "left"
+    },
+    {
+      right: 0,
+      top: 35,
+      text: "D",
+      cmd: "right"
+    },
+    {
+      left: 35,
+      top: 0,
+      text: "W",
+      cmd: "up"
+    },
+    {
+      left: 35,
+      bottom: 0,
+      text: "S",
+      cmd: "down"
+    }
+  ];
+  //Four navigation buttons
+  var navBtns = [];
+
+  if (options.navButtonsEnabled) {
+    for (var i in navBtnConfig) {
+      var btn = document.createElement("button");
+      options.hideonload && (btn.disabled = "disabled");
+      btn.style.cssText =
+        "position:absolute;" +
+        (navBtnConfig[i].left === undefined
+          ? "right:" + navBtnConfig[i].right
+          : "left:" + navBtnConfig[i].left) +
+        "px;width:30px;height:30px;" +
+        (navBtnConfig[i].top === undefined
+          ? "bottom:" + navBtnConfig[i].bottom
+          : "top:" + navBtnConfig[i].top) +
+        "px;";
+      btn.innerHTML = navBtnConfig[i].text;
+
+      navBtns.push(btn);
+      btn.onclick = (function(cmd) {
+        return function(e) {
+          move.call(div, cmd);
+        };
+      })(navBtnConfig[i].cmd);
+      ctrldiv.appendChild(btn);
+    }
+  }
+
+  var toggleBtn = document.createElement("button");
+  toggleBtn.style.cssText =
+    "width:30px;height:30px;position:absolute;right:35px;top:35px;";
+  toggleBtn.innerHTML = "G";
+
+  toggleBtn.onclick = function(e) {
+    toggle();
+  };
+  ctrldiv.appendChild(toggleBtn);
+
+  //Move the half-opacity div
+  var move = function(direction, step) {
+    if ("none" == this.style.display) return;
+
+    if (!+step) {
+      step = 1;
     }
 
-    function bindEvent(ele,evt,func){
-        if(document.addEventListener)
-        {
-            ele.addEventListener(evt,func,false);
-        }else{
-            ele.attachEvent('on' + evt ,func);
-        }
+    if (/(left|right)/i.test(direction)) {
+      var left = parseInt(getStyle(this, "left"));
+      left = "left" == direction ? left - step : left + step;
+      this.style.left = left + "px";
+    } else if (/(up|down)/i.test(direction)) {
+      var top = parseInt(getStyle(this, "top"));
+      top = "up" == direction ? top - step : top + step;
+      this.style.top = top + "px";
     }
+  };
 
-    //Load options
-    for (var i in defaultOptions) {
-        options[i] = (undefined === options[i]) ? defaultOptions[i] : options[i];
-    }
-    //Check options.ctrlPanelPosition
-    if (!/(NE|NW|SW|SE)/.test(options.ctrlPanelPosition)) {
-        options.ctrlPanelPosition = options.ctrlPanelPosition.ctrlPanelPosition;
-    }
-
-    //Half-opacity div
-    var div = document.createElement('div');
-
-    div.style.display=options.hideonload ? 'none' : 'block';
-    div.style.position='absolute';
-
-   div.style.zIndex=options.css.zIndex||10000;
-   div.style.width=options.css.width||"100%";
-   div.style.height=options.css.height||"100%";
-   div.style.left=0;
-   div.style.top=0;
-   div.style.background="url(" + imgSrc + ") left top no-repeat";
-    for (var e in options.css) {
-        if (options.css.hasOwnProperty(e)){
-            div.style[e] = options.css[e];
-        }
-    }
-
-    //Show or hide
-    var toggle = function() {
-        if ('none' == div.style.display) {
-            div.style.display = 'block';
-            for (var i in navBtns) {
-                navBtns[i].disabled = '';
-            }
-        } else {
-            div.style.display = 'none';
-            for (var i in navBtns) {
-                navBtns[i].disabled = 'disabled';
-            }
-        }
-    };
-    //With buttons
-    var ctrldiv = document.createElement('div');
-    var ctrldivPosCssText;
-    switch (options.ctrlPanelPosition) {
-        case "NE":
-            ctrldivPosCssText = "right:0;top:0;";
-            break;
-        case "NW":
-            ctrldivPosCssText = "left:0;top:0;";
-            break;
-        case "SE":
-            ctrldivPosCssText = "right:0;bottom:0;";
-            break;
-        case "SW":
-            ctrldivPosCssText = "left:0;bottom:0;";
-            break;
-    }
-    ctrldiv.style.cssText = "width:100px;height:100px;position:fixed;_position:absolute;" + ctrldivPosCssText + "z-index:" + ((1 + options.zIndex) | 0);
-
-    var navBtnConfig = [{
-            left: 0,
-            top: 35,
-            text: 'A',
-            cmd: 'left'
-        }, {
-            right: 0,
-            top: 35,
-            text: 'D',
-            cmd: 'right'
-        }, {
-            left: 35,
-            top: 0,
-            text: 'W',
-            cmd: 'up'
-        }, {
-            left: 35,
-            bottom: 0,
-            text: 'S',
-            cmd: 'down'
-        }
-    ];
-    //Four navigation buttons
-    var navBtns = [];
-
-    if (options.navButtonsEnabled) {
-        for (var i in navBtnConfig) {
-            var btn = document.createElement('button');
-            options.hideonload && (btn.disabled = 'disabled');
-            btn.style.cssText = "position:absolute;" + ((navBtnConfig[i].left === undefined) ? "right:" + navBtnConfig[i].right : "left:" + navBtnConfig[i].left) + "px;width:30px;height:30px;" + ((navBtnConfig[i].top === undefined) ? "bottom:" + navBtnConfig[i].bottom : "top:" + navBtnConfig[i].top) + "px;";
-            btn.innerHTML = navBtnConfig[i].text;
-
-            navBtns.push(btn);
-            btn.onclick = (function(cmd) {
-                return function(e) {
-                    move.call(div, cmd);
-                };
-            })(navBtnConfig[i].cmd);
-            ctrldiv.appendChild(btn);
-        }
-    }
-
-    var toggleBtn = document.createElement('button');
-    toggleBtn.style.cssText = "width:30px;height:30px;position:absolute;right:35px;top:35px;"
-    toggleBtn.innerHTML = "G";
-
-    toggleBtn.onclick = function(e) {
-        toggle();
-    };
-    ctrldiv.appendChild(toggleBtn);
-
-    //Move the half-opacity div
-    var move = function(direction,step) {
-        if ('none' == this.style.display) return;
-        
-        if( !+step ){
-                step = 1;
-        }
-
-        if (/(left|right)/i.test(direction)) {
-            var left = parseInt(getStyle(this,'left'));
-            left = ('left' == direction) ? left - step : left + step;
-            this.style.left = left + "px";
-        } else if (/(up|down)/i.test(direction)) {
-            var top = parseInt(getStyle(this,'top'));
-            top = ('up' == direction) ? top - step : top + step;
-            this.style.top = top + "px";
-        }
-
-    };
-
-    options.hotkeysEnabled && (bindEvent(document,'keydown',function(e) {
-        e = e || window.event;
-        switch (e.keyCode) {
-            case 65: //left
-                move.call(div, 'left',e.shiftKey?options.bigStep:1);
-                break;
-            case 87: //up
-                move.call(div, 'up',e.shiftKey?options.bigStep:1);
-                break;
-            case 68: //right
-                move.call(div, 'right',e.shiftKey?options.bigStep:1);
-                break;
-            case 83: //down
-                move.call(div, 'down',e.shiftKey?options.bigStep:1);
-                break;
-            case 71: //toggle
-                toggle();
-                break;
-            default:
-                ;
-        }
-    }));
-    document.body.appendChild(div);
-    document.body.appendChild(ctrldiv);
-
-};
+  options.hotkeysEnabled &&
+    bindEvent(document, "keydown", function(e) {
+      e = e || window.event;
+      switch (e.keyCode) {
+        case 65: //left
+          move.call(div, "left", e.shiftKey ? options.bigStep : 1);
+          break;
+        case 87: //up
+          move.call(div, "up", e.shiftKey ? options.bigStep : 1);
+          break;
+        case 68: //right
+          move.call(div, "right", e.shiftKey ? options.bigStep : 1);
+          break;
+        case 83: //down
+          move.call(div, "down", e.shiftKey ? options.bigStep : 1);
+          break;
+        case 71: //toggle
+          toggle();
+          break;
+        default:
+      }
+    });
+  document.body.appendChild(div);
+  document.body.appendChild(ctrldiv);
+}
